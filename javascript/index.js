f534f70195bb6e62dbad29f0b17539dc62cc3a2f
HanLander <157143716+HanLander@users.noreply.github.com>
2024-05-27 15:47:22 +0100

    Added basic javascript to the project

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
 
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "HH:mm:ss:SSS [<small>]A[</small>]"
+  );
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "HH:mm:ss:SSS [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1);
