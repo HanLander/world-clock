d43abbf94e4f169fce2f282e9fe0e3904e8f5769
HanLander <157143716+HanLander@users.noreply.github.com>
2024-05-28 20:59:16 +0100

    Added local time functionality

--- javascript/index.js
+++ javascript/index.js
@@ -6,7 +6,7 @@ function updateTime() {
 
   londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
   londonTimeElement.innerHTML = londonTime.format(
-    "HH:mm:ss:SSS [<small>]A[</small>]"
+    "HH:mm:ss [<small>]A[</small>]"
   );
 
   let sydneyElement = document.querySelector("#sydney");
@@ -16,29 +16,35 @@ function updateTime() {
 
   sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
   sydneyTimeElement.innerHTML = sydneyTime.format(
-    "HH:mm:ss:SSS [<small>]A[</small>]"
+    "HH:mm:ss [<small>]A[</small>]"
   );
 }
 
-updateTime();
-setInterval(updateTime, 1);
-
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
     <div class="city">
       <div>
-        <h2>${cityName} 🇦🇺</h2>
+        <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
       </div>
-      <div class="time">${cityTime.format(
-        "HH:mm:ss:SSS"
-      )} <small>${cityTime.format("A")} </small></div>
-    </div> `;
+      <div class="time">${cityTime.format("HH:mm:ss")} <small>${cityTime.format(
+    "A"
+  )} </small></div>
+    </div> 
+ <a href="/">All cities</a>
+    `;
 }
+
+updateTime();
+setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
